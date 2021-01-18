7d597161a0c5f1b1af4faa89299bbfc0691a676a
MURA199918 <muralisaranr18@gmail.com>
2021-01-18 11:52:30 +0530

    [Murali]Refactored for greeting app to find a greeting message by ID in the repository

--- src/main/java/com/mygreetingapp/controller/GreetingController.java
+++ src/main/java/com/mygreetingapp/controller/GreetingController.java
@@ -4,10 +4,7 @@ import com.mygreetingapp.model.Greeting;
 import com.mygreetingapp.model.User;
 import com.mygreetingapp.service.IGreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
